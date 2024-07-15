960ff86f04aac74dd67b0cb9a7383f59169cca62
majolivero <majolivero28@gmail.com>
2024-07-15 11:42:32 -0500

    Correciones en ejercicio 1 seccion 1

--- Seccion1/Ejercicio1/seccion1.js
+++ Seccion1/Ejercicio1/seccion1.js
@@ -24,18 +24,19 @@ const questionnaire = (questions) => {
           }
         });
         alert(`Las respuestas correctas son:
-            Pregunta 1: ${arrayAnswer[0]}
-            Explicación: Si, en JavaScript puedes acceder a una variable global desde un scope local. Las variables globales son accesibles en cualquier parte del código, incluyendo dentro de funciones, bloques y otros scopes locales.
-            Pregunta 2: ${arrayAnswer[1]}
-            Explicación: Si, en JavaScript puedes acceder a una variable global desde un scope de bloque. Las Variables globales son accesibles en cualquier parte del código, incluyendo dentro de bloques definidos por llaves {} como ciclos for y condicionales if, entre otros.
-            Pregunta 3: ${arrayAnswer[2]}
-            Explicación: No, en JavaScript no puedes acceder a una variable local desde un scope global. Las variables locales, que se definen dentro de una función o bloque, están limitadas a ese scope específico y no son accesibles fuera de él.
-            Pregunta 4: ${arrayAnswer[3]}
-            Explicación: Si, en JavaScript puedes acceder a una variable local desde un scope de bloque si la variable está definida en un scope que engloba al bloque. Sin embargo, las variables definidas dentro de un bloque solo son accesibles dentro de ese mismo bloque si se usan let o const.
-            Pregunta 5: ${arrayAnswer[4]}
-            Explicación: No, en JavaScript no puedes acceder a una variable de bloque desde un scope global. Las variables definidas dentro de un bloque con let o const tienen un alcance de bloque, lo que significa que solo son accesibles dentro del bloque donde fueran definidas. Fuera de ese bloque, dichas variables no existen.
-            Pregunta 6: ${arrayAnswer[5]}
-            Explicación: Si, puedes acceder a una variable de bloque desde un scope local en JavaScript siempre que la variable de bloque esté dentro del mismo scope local o un scope que englobe al scope local. Las variables de bloque, definidas con let o const, son accesibles dentro del bloque donde se declaran y en cualquier bloque anidado dentro de ese scope. 
+            Pregunta 1: ${arrayAnswer[0]};
+            Explicación:  En JavaScript puedes acceder a una variable global desde un scope local. Las variables globales son accesibles en cualquier parte del código, incluyendo dentro de funciones, bloques y otros scopes locales.`)
+            alert(`Pregunta 2: ${arrayAnswer[1]};
+            Explicación: En JavaScript puedes acceder a una variable global desde un scope de bloque. Las variables globales son accesibles en cualquier parte del código, incluyendo dentro de bloques definidos por llaves {} como ciclos for y condicionales if, entre otros.`)
+            alert(`Pregunta 3: ${arrayAnswer[2]}
+            Explicación: En JavaScript no puedes acceder a una variable local desde un scope global. Las variables locales, que se definen dentro de una función o bloque, están limitadas a ese scope específico y no son accesibles fuera de él.`)
+            alert(`Pregunta 4: ${arrayAnswer[3]}
+            Explicación: En JavaScript puedes acceder a una variable local desde un scope de bloque si la variable está definida en un scope que engloba al bloque. Sin embargo, las variables definidas dentro de un bloque solo son accesibles dentro de ese mismo bloque si se usan let o const.`)
+            alert(`Pregunta 5: ${arrayAnswer[4]}
+            Explicación: En JavaScript no puedes acceder a una variable de bloque desde un scope global. Las variables definidas dentro de un bloque con let o const tienen un alcance de bloque, lo que significa que solo son accesibles dentro del bloque donde fueran definidas. Fuera de ese bloque, dichas variables no existen.`)
+            alert(`Pregunta 6: ${arrayAnswer[5]}
+            Explicación: En JavaScript puedes acceder a una variable de bloque desde un scope local siempre que la variable de bloque esté dentro del mismo scope local o un scope que englobe al scope local.
+            
             Tu puntaje total es:  ${score} puntos`);
     }
     catch (error){
